fa16bfe832b13138c2e661700315ca4f360afa64
Y10Design-HNaudie <72567116+Y10Design-HNaudie@users.noreply.github.com>
2021-01-18 02:15:07 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+
+  var instance = M.Sidenav.getInstance(elem);
+
+
+
+
+
+  
